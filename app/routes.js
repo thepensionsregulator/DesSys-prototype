71809e2a50d88eda2a080fe834ca47527f1b26b9
JodiB-TPR <jodi.bennett@tpr.gov.uk>
2022-01-27 10:24:49 +0000

    Added Vicky Teinaki/'s code to make it easy to check what I'm doing. Updated app/config.js, app/routes.js, and server.js to log to Terminal.

--- app/routes.js
+++ app/routes.js
@@ -3,4 +3,14 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
+// GET SPRINT (or VERSION) NAME - useful for relative templates
+router.use('/', (req, res, next) => {
+    res.locals.currentURL = req.originalUrl; // Current screen
+    res.locals.prevURL = req.get('Referrer'); // Previous screen
+
+    console.log('Previous page is ' + res.locals.prevURL + " and Current page is " + res.locals.currentURL);
+
+    next();
+})
+
 module.exports = router
